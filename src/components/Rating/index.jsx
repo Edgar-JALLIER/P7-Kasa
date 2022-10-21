953a727e008f638d440de208a8e8475ff92425cf
Edgar JALLIER <edgarjallier@gmail.com>
2022-10-21 13:46:46 +0200

    Ajout du responsive pour la description des logements

--- src/components/Rating/index.jsx
+++ src/components/Rating/index.jsx
@@ -6,7 +6,7 @@ function Rating (rateValue) {
     const range = [1,2,3,4,5]
     const value = Object.values(rateValue)
     return (
-        <div className="Rating">
+        <div>
             {range.map ((rangeElement) => 
             (value >= rangeElement ? <span key={rangeElement.toString()}><img src={RedStar} alt="Etoile rouge"/></span> : <span key={rangeElement.toString()}><img src={GreyStar} alt="Etoile grise"/></span>))}
         </div>
